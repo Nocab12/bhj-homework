bff1a7b0025668038a6ee0504fa0053c639ef41c
Denis Berezin <nocab@yandex.ru>
2019-09-09 23:01:26 +0300

    исправление менюшки

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,13 +1,17 @@
-const arrMenuSub = Array.from(document.querySelectorAll('li.menu__item > ul'));
-const arrMenuItem = Array.from(document.querySelectorAll('li.menu__item > a'));
+const menuLinks = document.querySelectorAll('.menu__link');
 
-arrMenuItem[1].onclick = function(){
-	arrMenuSub[0].className = "menu menu_sub menu_active";
-	arrMenuSub[1].className = "menu menu_sub";
-	return false;
-};
-arrMenuItem[5].onclick = function(){
-	arrMenuSub[1].className = "menu menu_sub menu_active";
-	arrMenuSub[0].className = "menu menu_sub";
-	return false;
-};
+for (let link of Array.from(menuLinks)) {
+    link.onclick = function () {
+
+        const parent = link.parentElement;
+        if (parent.querySelector('.menu_sub').classList.contains('menu_active')) {
+            parent.querySelector('.menu_sub').classList.remove('menu_active');
+        } else {
+            parent.querySelector('.menu_sub').classList.add('menu_active'); 
+        }
+
+        if (link.closest('.menu_main')) {
+            return false
+        }
+    }
+}
