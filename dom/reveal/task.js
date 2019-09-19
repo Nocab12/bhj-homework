ba8c980a66cb877a4e517742751044bbe40db636
Denis Berezin <nocab@yandex.ru>
2019-09-19 20:06:05 +0300

    homework4

---
+++ dom/reveal/task.js
@@ -0,0 +1,16 @@
+let revealElems = document.querySelectorAll('.reveal');
+
+
+
+for (let el of revealElems){
+  window.addEventListener('scroll', ()=>{
+  let viewportHeight = window.innerHeight;
+  let elemBotoom = el.getBoundingClientRect().bottom;
+  let elemTop = el.getBoundingClientRect().top
+  if ( elemBotoom <= viewportHeight && elemTop < viewportHeight && elemBotoom >= 0) {
+    el.classList.add('reveal_active')
+  } else {
+    el.classList.remove('reveal_active')
+  }
+})
+}
